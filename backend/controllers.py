248afa2f8f434b5c91625196cf3d597885b444a3
22f3001013 <22f3001013@ds.study.iitm.ac.in>
2024-07-09 23:44:12 +0530

    commit 09-07-2024

--- backend/controllers.py
+++ backend/controllers.py
@@ -11,20 +11,20 @@ def user_login():
     if request.method=="POST":
         uname=request.form.get("uname")
         pwd=request.form.get("pwd")
-        admin_user = Admin.query.filter_by(user_name=uname, pass_hash=pwd).first() #Get existing user matched
-        sponser_user=Sponser.query.filter_by(user_name=uname, pass_hash=pwd)
-        influencer_user=Influencer.query.filter_by(user_name=uname, pass_hash=pwd)
-        if admin_user and admin_user.is_admin:
-            return render_template("admin_dashboard_info.html")
+        user = User.query.filter_by(user_name=uname, pwd=pwd).first() #Get existing user matched
+        sponser_user = Sponser.query.filter_by(user_name=uname, pwd=pwd).first()
+        if user and user.type == "admin" :
+           campaign_summary = fetch_campaigns() #Calling
+           return render_template("admin_dashboard_info.html", campaigns = campaign_summary )
         
-        elif sponser_user and sponser_user.is_sponser:
-           return render_template("sponser_dashboard_profile.html", username = sponser_user.user_name)
+        elif user and user.type == "general" :
+            return render_template("influencer_dashboard_profile.html", username = user.full_name)
         
-        elif influencer_user and influencer_user.is_influencer:
-           return render_template("influencer_dashboard_profile.html", username = influencer_user.user_name)
-            
-        else:
-            return render_template("login.html",  msg="Invaid Credentials!!")
+        elif sponser_user:
+           return render_template("sponser_dashboard_profile.html") 
+        
+        return render_template("login.html",  msg="Invaid Credentials!!")
+   
     else:
        return render_template("login.html",msg="")
     
@@ -33,20 +33,19 @@ def user_login():
 def influencer_signup():
     if request.method=="POST":
         email=request.form.get("email")
-        full_name=request.form.get("full_name")
+        full_name=request.form.get("fullname")
         uname=request.form.get("uname")
         pwd=request.form.get("pwd")
         category=request.form.get("category")
         niche=request.form.get("niche")
-        user = Influencer.query.filter_by(user_name=uname).first() #Get existing user matched
+        user = User.query.filter_by(user_name=uname).first() #Get existing user matched
         if not user:
-            new_user=Influencer(email=email,user_name=uname, pass_hash=pwd, name=full_name,  niche=niche, category=category)
+            new_user=User(email=email,user_name=uname, pwd=pwd, full_name=full_name,  niche=niche, category=category)
             db.session.add(new_user)
             db.session.commit()
             return render_template("login.html", msg="")
         else:
-
-            return render_template("influencer.html", msg="User already exists!")
+           return render_template("influencer.html", msg="User already exists!")
     else:
        return render_template("influencer.html",msg="")
 
@@ -55,15 +54,29 @@ def influencer_signup():
 def sponser_signup():
     if request.method=="POST":
         email=request.form.get("email")
-        fname=request.form.get("fname")
-        lname=request.form.get("lname")
+        company_name=request.form.get("company_name")
         uname=request.form.get("uname")
         pwd=request.form.get("pwd")
         indus=request.form.get("indus")
-        company_name=request.form.get("company_name")
-    return render_template("sponser.html")
+        user = Sponser.query.filter_by(user_name=uname).first() #Get existing user matched
+        if not user:
+            new_user=Sponser(email=email,user_name=uname, pwd=pwd, industry =indus, company_name=company_name)
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template("login.html", msg="")
+        else:
+            return render_template("sponser.html", msg="User already exists!")
+    else:
+       return render_template("sponser.html", msg="")
 
-
+#User defined function
+def fetch_campaigns():
+    campaigns=Campaigns.query.filter_by(visibility = "public" ).all()
+    campaign_list = {}
+    for campaign in campaigns:
+        if campaign.id not in campaign_list.keys():
+            campaign_list[campaign.id] = [campaign.name, campaign.start_date]
+    return campaign_list
 
 
 
--- backend/models.py
+++ backend/models.py
@@ -3,96 +3,54 @@ from datetime import datetime
 
 db=SQLAlchemy() #Instance of SQLALchemy
 
-class Admin(db.Model):
-    __tablename__="admin"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(64), nullable=False)
-    user_name = db.Column(db.String(32), unique=True, nullable=False)
-    pass_hash = db.Column(db.String(512), nullable=False)
-    is_admin= db.Column(db.Boolean, default=True)
-
-class Influencer(db.Model):
-    __tablename__ = "influencer"
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(100), nullable=False)
-    user_name = db.Column(db.String(32), nullable=False)
-    pass_hash = db.Column(db.String(512), nullable=False)
-    name  = db.Column(db.String(64), nullable=False)
-    niche = db.Column(db.String(64), nullable=False)
-    category = db.Column(db.String(64), nullable=False)
-    is_influencer = db.Column(db.Boolean, default=True)
-
-
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    email = db.Column(db.String)
+    full_name = db.Column(db.String, nullable = False)
+    user_name = db.Column(db.String, unique = True, nullable = False)
+    pwd = db.Column(db.String, nullable = False)
+    category = db.Column(db.String, nullable = False, default="null")
+    niche = db.Column(db.String, nullable = False, default="null")
+    type = db.Column(db.String, nullable = False, default = "general")
+    ad_request = db.relationship("Ad_request", backref="user")
 
 class Sponser(db.Model):
-    __tablename__="sponser"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    user_name = db.Column(db.String(32), unique=True, nullable=False)
-    pass_hash = db.Column(db.String(512), nullable=False)
-    name = db.Column(db.String(64), nullable=False)
-    industry = db.Column(db.String(64), nullable=False)
-    company_name = db.Column(db.String(64), nullable=False)
-    budget = db.Column(db.Integer, nullable=False)
-    is_sponser = db.Column(db.Boolean, default=True)
+    __tablename__ = "sponser"
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    company_name = db.Column(db.String, nullable = False)
+    user_name = db.Column(db.String, unique = True, nullable = False)
+    pwd = db.Column(db.String, nullable = False)
+    industry = db.Column(db.String, nullable = False)
+    budget = db.Column(db.Float, nullable = False, default = 1000.0)
+    campaign = db.relationship("Campaigns", backref="sponser")
 
 
 
 class Campaigns(db.Model):
     __tablename__ = "campaigns"
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(64), nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
-
-    
-
-class Create_Campaign(db.Model):
-    __tablename__ = "create_campaign"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    niche = db.Column(db.String, nullable=False)
-    date = db.Column(db.Date, nullable = False)
-    camp_id = db.Column(db.Integer, db.ForeignKey("campaigns.id"), nullable=False)
-    sponser_id = db.Column(db.Integer, db.ForeignKey("sponser.id"), nullable=False)
-    campaigns = db.relationship("Campaigns", backref="create_campaign")
-    sponser = db.relationship("Sponser", backref="create_campaign")
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String, nullable = False)
+    description = db.Column(db.Text, nullable = False)
+    start_date = db.Column(db.Date, nullable = False)
+    end_date = db.Column(db.Date, nullable = False)
+    budget = db.Column(db.Float, nullable = False)
+    visibility = db.Column(db.String, nullable = False, default="public") 
+    goals = db.Column(db.Text, nullable = False)
+    sponser_id = db.Column(db.Integer, db.ForeignKey('sponser.id'))
+    ad_request = db.relationship("Ad_request", backref="campaigns")
 
 
-class Ad_Request(db.Model):
+class Ad_request(db.Model):
     __tablename__ = "ad_request"
-    id = db.Column(db.Integer, primary_key=True)
-    messages = db.Column(db.String, nullable=False)
-    requirements = db.Column(db.String, nullable=False)
-    payment_amount = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String, nullable=False)
-    camp_id = db.Column(db.Integer, db.ForeignKey("campaigns.id"), nullable=False)
-    influencer_id = db.Column(db.Integer, db.ForeignKey("influencer.id"), nullable=False)
-    campaigns = db.relationship("Campaigns", backref='ad_request')
-    influencer = db.relationship("Influencer", backref="ad_request")
-
-
-class Goals(db.Model):
-    __tablename__ = "goals"
-    id = db.Column(db.Integer, primary_key=True)
-    camp_description = db.Column(db.String, nullable=False)
-    budget = db.Column(db.Integer, nullable=False)
-    visibility = db.Column(db.String, nullable=False, default="public")
-    goals = db.Column(db.String, nullable=False)
-    camp_id = db.Column(db.Integer, db.ForeignKey("campaigns.id"), nullable=False)
-    ad_id = db.Column(db.Integer, db.ForeignKey("ad_request.id"), nullable=False)
-    campaigns = db.relationship("Campaigns", backref="goals")
-    ad_request = db.relationship("Ad_Request", backref="goals")
-
-
-
-
-
-
-
-    
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    message =  db.Column(db.Text, nullable = False)
+    payment_amount = db.Column(db.Float, nullable = False)
+    requirements = db.Column(db.String, nullable = False)
+    status = db.Column(db.String, nullable = False, default = "Pending")
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaigns.id'))
+    influencer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 
